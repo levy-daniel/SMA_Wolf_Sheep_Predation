a9605b5320a0b57505ec9115f73db2ee7d98c4d7
Pierre-Alexandre Klein <pierre-alexandre.klein@student-cs.fr>
2023-03-08 14:45:58 +0100

    minor updates

--- 0_prey_predator_start/code/prey_predator/server.py
+++ 0_prey_predator_start/code/prey_predator/server.py
@@ -17,14 +17,14 @@ def wolf_sheep_portrayal(agent):
         portrayal = {"Shape": "circle",
                  "Filled": "true",
                  "Layer": 0,
-                 "Color": "red",
+                 "Color": "blue",
                  "r": 0.5}
 
     elif type(agent) is Wolf:
         portrayal = {"Shape": "circle",
                  "Filled": "true",
                  "Layer": 0,
-                 "Color": "blue",
+                 "Color": "red",
                  "r": 0.5}        # ... to be completed
 
     elif type(agent) is GrassPatch:
@@ -48,7 +48,7 @@ def wolf_sheep_portrayal(agent):
 
 canvas_element = CanvasGrid(wolf_sheep_portrayal, 20, 20, 500, 500)
 chart_element = ChartModule(
-    [{"Label": "Wolves", "Color": "#AA0000"}, {"Label": "Sheep", "Color": "#666666"}]
+    [{"Label": "Wolves", "Color": "#AA0000"}, {"Label": "Sheep", "Color": "#0000FF"}]
 )
 
 height=20
@@ -57,14 +57,14 @@ width=20
 model_params = {
     "height" : height,
     "width" : width,
-    "initial_sheep" :  UserSettableParameter("slider", "Initial Number of Sheep", value=100, min_value=10, max_value=200, step=1),
-    "initial_wolves" :  UserSettableParameter("slider", "Initial Number of Wolf", value=25, min_value=10, max_value=200, step=1),
-    "sheep_reproduce" :  UserSettableParameter("slider", "Probability of sheep reproduce", value=0.04, min_value=0.01, max_value=0.2, step=0.01),
+    "initial_sheep" :  UserSettableParameter("slider", "Initial Number of Sheep", value=50, min_value=10, max_value=200, step=1),
+    "initial_wolves" :  UserSettableParameter("slider", "Initial Number of Wolf", value=20, min_value=10, max_value=200, step=1),
+    "sheep_reproduce" :  UserSettableParameter("slider", "Probability of sheep reproduce", value=0.06, min_value=0.01, max_value=0.2, step=0.01),
     "wolf_reproduce" :  UserSettableParameter("slider", "Probability of wolf reproduce", value=0.05, min_value=0.01, max_value=0.2, step=0.01),
-    "wolf_gain_from_food" :  UserSettableParameter("slider", "Energy won when eating 1 sheep", value=20, min_value=1, max_value=30, step=1),
+    "wolf_gain_from_food" :  UserSettableParameter("slider", "Energy won when eating 1 sheep", value=10, min_value=1, max_value=30, step=1),
     "grass" :  WolfSheep.grass,
     "grass_regrowth_time" :  UserSettableParameter("slider", "Grass regrowth time", value=30, min_value=10, max_value=100, step=1),
-    "sheep_gain_from_food" :  UserSettableParameter("slider", "Energy won when eating grass", value=4, min_value=1, max_value=30, step=1)   
+    "sheep_gain_from_food" :  UserSettableParameter("slider", "Energy won when eating grass", value=10, min_value=1, max_value=30, step=1)   
     # ... to be completed  
 }
 
